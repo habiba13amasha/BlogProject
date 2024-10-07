ff1330f2b695282fa8cc78b8eeaf5d85bba50fee
habiba <habibaamasha32@gmail.com>
2024-10-07 04:16:09 +0300

    connect database

--- server(api)/index.js
+++ server(api)/index.js
@@ -1,6 +1,13 @@
-const express = require("express");
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config() // يجب أن يكون هذا في بداية الكود
+
 const app=express()
 
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log("Database is Connected")}).catch((err)=>{console.log("connection failed")})
 
 
 app.listen(3000, () =>console.log("Server running on port 3000") );
