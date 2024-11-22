781b735ec254d96f246d8da4bac7533b2acf8e44
habiba <habibaamasha32@gmail.com>
2024-11-23 01:03:21 +0200

    add edit functionality to comment component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react"
 import moment from "moment"
 import {FaThumpsUp} from "react-icons/fa"
 import { useSelector } from "react-redux"
-export default function Comment({comment,onLike}) {
+import{Button, Textarea} from "flowbite-react"
+export default function Comment({comment,onLike,onEdit}) {
   const {currentUser}=useSelector(state=>state.user)
-    const [user,setUser]=useState({})
+  const [user,setUser]=useState({})
+  const[isEditing,setIsEditing]=useState(false)
+  const [editedContent,setEditedContent]=useState(comment.content)
     useEffect(()=>{
       const getUser=async()=>{
         try{
@@ -19,6 +22,30 @@ export default function Comment({comment,onLike}) {
       }
       getUser()
     },[comment])
+
+    const handleEdit=async()=>{
+      setIsEditing(true)
+      setEditedContent(comment.content)
+      
+    }
+    const handleSave=async()=>{
+      try {
+        const res=await fetch(`/api/comment/editComment/${comment._id}`,{
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({content: editedContent})
+        })
+        const data=await res.json()
+        if(res.ok){
+          setIsEditing(false)
+          onEdit(comment,editedContent)
+        }
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
       <div className="mr-3 flex-shrink-0">
@@ -29,18 +56,40 @@ export default function Comment({comment,onLike}) {
           <span className="font-bold text-xs mr-1 truncate">{user ? `@${user.userName}` : "anonymous user"}</span>
           <span className="text-xs text-gray-500">{moment(comment.createdAt).fromNow()}</span>
         </div>
-        <p className="text-gray-500 pb-2">{comment.content}</p>
-        <div className="flex items-center pt-2 text-xs border-t max-w-fit gap-2 dark:text-gray-700">
-          <button type="button" onClick={()=>onLike(comment._id)}
-           className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id)&& `!text-blue-500` } `}>
-            <FaThumpsUp className="text-sm"/>
-          </button>
-          <p className="text-gray-400">
-            {
-              comment.numberOfLikes >0 && comment.numberOfLikes + " " + {comment.numberOfLikes === 1 ? "like" :"likes"}
-            }
-          </p>
-        </div>
+        {isEditing ?(
+          <>
+          <Textarea value={editedContent} onChange={(e)=>setEditedContent(e.target.value)}
+           className="mb-2"
+          />
+          <div className="flex justify-end text-xs gap-2">
+            <Button type="button" size="sm" gradientDuoTone="purpleToBlue" onClick={handleSave}>Save</Button>
+            <Button type="button" size="sm" gradientDuoTone="purpleToBlue" onClick={()=>setIsEditing(false)}>Cancel</Button>
+          </div>
+          </>
+        ):(
+          <>
+          <p className="text-gray-500 pb-2">{comment.content}</p>
+          <div className="flex items-center pt-2 text-xs border-t max-w-fit gap-2 dark:text-gray-700">
+            <button type="button" onClick={()=>onLike(comment._id)}
+            className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id)&& `!text-blue-500` } `}>
+              <FaThumpsUp className="text-sm"/>
+            </button>
+            <p className="text-gray-400">
+              {
+                comment.numberOfLikes >0 && comment.numberOfLikes + " " + {comment.numberOfLikes === 1 ? "like" :"likes"}
+              }
+            </p>
+           {
+            currentUser &&( currentUser._id === comment.userId || currentUser.isAdmin)&& (
+              <button type="button" className="text-gray-400 hover:text-blue-500" onClick={handleEdit}>
+                Edit
+              </button>
+            )
+           }
+          </div>
+
+        </>
+        )}
       </div>
     </div>
   )
--- server(api)/routes/commentRoutes.js
+++ server(api)/routes/commentRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { createComment,getPostComments,likeComment } from "../controllers/commentController.js"
+import { createComment,getPostComments,likeComment,editComment } from "../controllers/commentController.js"
 
 const router=express.Router()
 
 router.post("/create-comment",verifyToken,createComment)
 router.get("/getPostComments/:postId",getPostComments)
 router.put("/likeComment/:commentId",verifyToken,likeComment)
+router.put("/editComment/:commentId",verifyToken,editComment)
 
 export default router
