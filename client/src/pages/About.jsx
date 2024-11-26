7137a345ec103e7844414a57afe456d128d4f834
habiba <habibaamasha32@gmail.com>
2024-11-26 05:25:53 +0200

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='mx-auto max-w-3xl p-3 text-center'>
+        <div >
+         <h1 className='text-center text-3xl my-7 font-semibold '>About Habiba`s Blog</h1>
+         <div className='text-md text-gray-500 flex flex-col gap-6'>
+          <p>
+            Habiba is a full stack developer and aspiring to learn more about web development. She has a passion for learning new technologies and shares her knowledge with others. She is currently working remotely and focusing on her personal projects. She has a blog where she posts about her experiences, learning, and projects. Habiba is also a certified JavaScript developer and has a strong foundation in HTML, CSS, and JavaScript. She is always eager to learn new skills and share her knowledge with others.
+          </p>
+          <p>
+            She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong foundation in HTML, CSS, and JavaScript. She is always eager to learn new skills and share her knowledge with others. She is currently working remotely and focusing on her personal projects. She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong foundation in HTML, CSS, and JavaScript. She is always eager to learn new skills and share her knowledge with others. She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong foundation in HTML, CSS, and JavaScript. She is always eager to learn new skills and share her knowledge with others. She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong foundation
+          </p>
+          <p>
+            She is always eager to learn new skills and share her knowledge with others. She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong foundation in HTML, CSS, and JavaScript. She is always eager to learn new skills and share her knowledge with others. She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong foundation in HTML, CSS, and JavaScript. She is always eager to learn new skills and share her knowledge with others. She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong foundation in HTML, CSS, and JavaScript. She is always eager to learn new skills and share her knowledge with others. She has a blog where she posts about her experiences, learning, and projects. She is also a certified JavaScript developer and has a strong
+          </p>
+         </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,6 +1,12 @@
-
+import CallToAction from "../components/CallToAction"
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="font-semibold text-3xl">Projects</h1>
+      <p className="text-gray-500 text-md">
+        Build fun and engaging projects while learning HTML,CSS,JS
+      </p>
+      <CallToAction/>
+    </div>
   )
 }
